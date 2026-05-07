c84778b7f432074f2da357814c60b7ef021e3fab
Lee7009201 <7009201@spsd.sk.ca>
2026-05-07 10:19:22 -0600

    test for bypassing cors error

--- api/stock.js
+++ api/stock.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+
+
   const { ticker } = req.query;
 
   const API_KEY = process.env.STOCK_API_KEY;
