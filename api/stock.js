dbb861066cd41c0d4bca7bdb2a8858ab48045b1f
Lee7009201 <7009201@spsd.sk.ca>
2026-06-03 10:16:31 -0600

    added a new function to link

--- api/stock.js
+++ api/stock.js
@@ -4,13 +4,20 @@ export default async function handler(req, res) {
 
 
   const { ticker } = req.query;
+  const { target } = req.query;
 
   const API_KEY = process.env.STOCK_API_KEY;
 
   // const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=5min&apikey=${API_KEY}`;
 
   // EODHD
-  const url = `https://eodhd.com/api/eod/${ticker}.US?api_token=${API_KEY}&fmt=json`
+  if (ticker = 'history') {
+    const url = `https://eodhd.com/api/eod/${ticker}.US?api_token=${API_KEY}&fmt=json`;
+  }
+  else if (ticker = 'current') {
+    const url = `https://eodhd.com/api/real-time/${ticker}.US?api_token=${API_KEY}&fmt=json`;
+
+  }
 
   try {
     const response = await fetch(url);
