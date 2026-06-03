2db2c13831e37317d353144f07baef995549497b
Lee7009201 <7009201@spsd.sk.ca>
2026-06-03 10:35:11 -0600

    debug

--- api/stock.js
+++ api/stock.js
@@ -1,6 +1,11 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allowed headers
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
 
 
   const { ticker, target } = req.query;
