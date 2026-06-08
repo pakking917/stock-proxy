f7099a6cf1b28f0bbf86a43740e1aa8b667c314a
pakking917 <80308233+pakking917@users.noreply.github.com>
2026-06-08 09:53:29 -0600

    debug

--- api/stock.js
+++ api/stock.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allowed headers
 
